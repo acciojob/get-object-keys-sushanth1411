c80ccbb11a21416c6d8b36a027bf0f6af58e298a
sushanth1411 <bt23cse167@iiitn.ac.in>
2026-07-04 11:09:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-//your JS code here. If required.
-const student={
-	name:"John",
+// your JS code here
+
+const student = {
+    name: "John"
 };
-function getKeys(obj){
-	return Object.keys(obj);
+
+function getKeys(obj) {
+    return Object.keys(obj);
 }
